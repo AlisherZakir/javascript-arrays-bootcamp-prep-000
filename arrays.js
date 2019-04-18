f735c8e3a036cf0bffb1800e7163a383895b198d
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 14:34:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-const addElementToBeginningOfArray(array, element)
+const addElementToBeginningOfArray = (array, element) => {
+  array.unshift(element);
+}
